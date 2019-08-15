63a1d4aac2770eb5de0793b7a59c09ae33f05f47
Eber Kachi <eber.nina96@gmail.com>
2019-08-15 05:29:52 -0400

    para convertir .py en .exe  pyinstaller -c y el nombre del archivo .py

--- ponerIP.py
+++ ponerIP.py
@@ -12,6 +12,7 @@ def menu(mensajes):
 
     print("•÷±‡±( 0. Salir )±‡±÷ ")
 
+os.system('color a')
 menu(["Crear archivo ip.txt","Poner Ip ", "Borrar Ip"])
 
 opcion=input()
@@ -25,13 +26,24 @@ elif opcion=="2":
     opcion=input()
     if opcion=="1":
         # ip por defecto
+        archivo=open('ip.txt','r+')
+        ipNumero=archivo.readline() 
+        print("estas es la ip "+ipNumero)
+        os.system(f'netsh interface ipv4 set address name="Wi-Fi" static 192.168.1.{ipNumero} 255.255.255.0 192.168.1.1')
+        # archivEscribir=open('ip.txt','w')
+        ipNumero = int(ipNumero) + 1 
+        archivo.write(str(ipNumero))
+        archivo.close()
+        print('finalizado..')
 
-    elif opcion =="2":
+    elif opcion == "2":
         # ip escrito
         print("Dijite tipo de IP ejm(192.168.1.*automatico)")
         tipoIp=input()
         print("Mascara ejm(/24)")
         tipoMascara=input()
+
+# raw_input()
     
 
 
